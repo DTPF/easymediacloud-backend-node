57b8b2d10af6e9ef8610b160a72c607cc3a4bcaa
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-22 14:04:32 +0100

    change: add messages

--- src/assets/locale/es.ts
+++ src/assets/locale/es.ts
@@ -12,32 +12,33 @@ export const spanishLang = {
 
   // Licenses
   'licenses_created-success': 'La licencia se ha creado correctamente',
-  'project-repeated': 'El nombre del proyecto ya existe en la base de datos',
   'licenses_create-directory-error': 'El directorio ya existe',
   'licenses_not-found': 'La licencia no existe en la base de datos del usuario',
   'licenses_get-license_error': 'Error al obtener la licencia',
   'licenses_get-license_success': 'Licencia obtenida correctamente',
   'licenses_get-my-licenses_success': 'Licencias obtenidas correctamente',
-  'license_not-enabled': 'La licencia no está activada',
-  'create-api-key-token-error': 'Error al crear el token de la licencia',
   'licenses_license-id-required': 'Falta el id de la licencia',
   'licenses_enabled-required': 'Falta el estado de la licencia',
   'licenses_enable-license_success': 'Licencia activada correctamente',
   'licenses_update-license_success': 'Licencia actualizada correctamente',
+  'license_not-enabled': 'La licencia no está activada',
+  'license-not-enabled': 'La licencia no está activada',
+  'license-not-found': 'La licencia no existe en la base de datos',
+  'license-offline': 'La licencia está offline',
+  'project-repeated': 'El nombre del proyecto ya existe en la base de datos',
+  'create-api-key-token-error': 'Error al crear el token de la licencia',
+  'api-key-not-found': 'Falta la Api-Key en la licencia',
 
   // Media
-  'license-not-found': 'La licencia no existe en la base de datos',
-  'api-key-not-found': 'Falta la Api-Key en la licencia',
-  'license-not-enabled': 'La licencia no está activada',
   'create-media-success': 'Archivo creado correctamente',
   'create-media-error': 'Error al crear la imágen',
-  'token-not-valid': 'El token no es válido',
   'media-protected': 'El archivo está protegido',
-
+  
   // Shared
   'server-error': 'Error del servidor',
   'data-required': 'Falta información en la petición',
   'media-not-exists': 'El archivo que buscas no existe.',
   'unauthorized': 'No tienes permisos para realizar esta acción',
+  'token-not-valid': 'El token no es válido',
   'not-verified': 'El usuario no está verificado',
 }

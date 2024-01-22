9ed2990271d8f38839e0ed4ac26ac7227f93f0a9
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-22 14:13:39 +0100

    change: rename interfaces and add suscription interface

--- src/interfaces/license.interface.ts
+++ src/interfaces/license.interface.ts
@@ -1,17 +1,27 @@
-import { User } from "./user.interface"
+import { IUser } from "./user.interface"
 
-export interface LicenseInterface {
+export interface ILicense {
   _id: string
-  userId?: User
+  userId?: IUser
   project: string
   apiKey?: string
   enabled?: boolean
+  online?: boolean
   size: string
+  totalFiles?: number
   __v: any
 }
 
-export interface ApiKeyInterface {
+export interface IApiKey {
   project: string
   email: string
   apiKey: string
+}
+
+interface ISuscription {
+  id: string
+  name: string
+  price: number
+  currency: string
+  maxSize: number
 }

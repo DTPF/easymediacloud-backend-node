153db1eb8bdcc22f4704fb3831cdb4c32f95520b
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-22 14:15:36 +0100

    change: rename interface

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response } from "express";
 import UserModel from "../core/user/models/user.model";
 import { adminRole } from "../utils/constants";
 import { responseKey, userKey } from "../core/responseKey";
-import { RequestUser, User } from "../interfaces/user.interface";
+import { IRequestUser, IUser } from "../interfaces/user.interface";
 import { ServerConfig } from "../config/config";
 import i18next from "i18next";
 const config: ServerConfig = require('../config/config')
@@ -15,8 +15,8 @@ export const auth_0 = auth({
   tokenSigningAlg: 'RS256'
 });
 
-export const is_verified = async (req: RequestUser, res: Response, next: NextFunction) => {
-  const findUser: User = await UserModel.findOne({ auth0Id: req.auth.payload.sub }).populate('licenses').lean().exec()
+export const is_verified = async (req: IRequestUser, res: Response, next: NextFunction) => {
+  const findUser: IUser = await UserModel.findOne({ auth0Id: req.auth.payload.sub }).populate('licenses').lean().exec()
   if (!findUser) {
     return res.status(404).send({ status: userKey.notFound, message: t('user-not-found'), key: 'is_verified' })
   }
@@ -28,8 +28,8 @@ export const is_verified = async (req: RequestUser, res: Response, next: NextFun
   next()
 }
 
-export const ensure_admin = async (req: RequestUser, res: Response, next: NextFunction) => {
-  const findUser: User = await UserModel.findOne({ auth0Id: req.auth.payload.sub }).populate('licenses').lean().exec()
+export const ensure_admin = async (req: IRequestUser, res: Response, next: NextFunction) => {
+  const findUser: IUser = await UserModel.findOne({ auth0Id: req.auth.payload.sub }).populate('licenses').lean().exec()
   if (!findUser) {
     return res.status(404).send({ status: userKey.notFound, message: t('user-not-found'), key: 'ensure_admin' })
   }
@@ -44,8 +44,8 @@ export const ensure_admin = async (req: RequestUser, res: Response, next: NextFu
   next()
 }
 
-export const hydrate = async (req: RequestUser, res: Response, next: NextFunction) => {
-  const findUser: User = await UserModel.findOne({ auth0Id: req.auth.payload.sub }).populate('licenses').lean().exec()
+export const hydrate = async (req: IRequestUser, res: Response, next: NextFunction) => {
+  const findUser: IUser = await UserModel.findOne({ auth0Id: req.auth.payload.sub }).populate('licenses').lean().exec()
   if (!findUser) {
     return res.status(404).send({ status: userKey.notFound, message: t('user-not-found'), key: 'hydrate' })
   }

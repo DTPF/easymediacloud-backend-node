3490dd83de89bbd1b28efa82bd4e7942205683f8
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-22 14:08:41 +0100

    change: add size transformed

--- src/core/media/models/media.model.ts
+++ src/core/media/models/media.model.ts
@@ -5,6 +5,8 @@ const MediaSchema = new Schema({
   directory: { type: String },
   url: { type: String, required: true },
   fileName: { type: String, required: true },
+  sizeT: { type: String, default: '0 B' },
+  size: { type: Number, default: 0 },
   enabled: { type: Boolean, default: true },
 }, {
   timestamps: true

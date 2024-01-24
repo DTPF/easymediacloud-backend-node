426fa5696aae9fd928a9dec2e4b35965617ed080
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-24 23:12:15 +0100

    change: add route to set online field

--- src/core/licenses/router/licenses.router.ts
+++ src/core/licenses/router/licenses.router.ts
@@ -11,6 +11,7 @@ api.
   get("/refresh-license-token/:licenseId", [auth_0, is_verified], controller.refreshLicenseToken).
   get("/get-my-licenses", [auth_0, is_verified], controller.getMyLicenses).
   patch("/enable-license", [auth_0, ensure_admin], controller.enableLicense).
+  patch("/set-online-license", [auth_0, is_verified], controller.setOnlineLicense).
   patch("/update-license-project", [auth_0, is_verified], controller.updateLicenseProject).
   //Pending
   delete("/delete-license", [auth_0, ensure_admin], controller.deleteLicense)

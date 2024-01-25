4e641c2f8154343f015a2d472e4d41130690f3dd
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-25 18:49:39 +0100

    change: add constants

--- src/core/subscriptions/subscriptionsConstants.ts
+++ src/core/subscriptions/subscriptionsConstants.ts
@@ -4,6 +4,7 @@ export const B10MB = 10485760
 export const B500MB = 524288000
 export const B5GB = 5368709120
 export const B20GB = 21474836480
+export const B100GB = 107374182400
 // Converted
 export const C500MB = convertBytes(B500MB)
 export const C5GB = convertBytes(B5GB)
@@ -12,3 +13,4 @@ export const C20GB = convertBytes(B20GB)
 export const FREE = 'free'
 export const BASIC = 'basic'
 export const PREMIUM = 'premium'
+export const CUSTOM = 'custom'

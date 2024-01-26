ff69f630c815be859e89a9f2022f3e0f8b41f2d5
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-27 00:04:32 +0100

    change: add requests fields

--- src/core/subscriptions/models/subscription.model.ts
+++ src/core/subscriptions/models/subscription.model.ts
@@ -1,16 +1,20 @@
+import moment from "moment";
+import { convertBytes } from "../../../utils/getFolderSize";
 import { LICENSE_MODEL, SUBSCRIPTION_MODEL, USER_MODEL } from "../../modelsConstants";
+import { B500MB, FREE, REQUESTS_PER_MONTH } from "../subscriptionsConstants";
 const { Schema, model } = require('mongoose');
 
 const SubscriptionSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: USER_MODEL, required: true },
   license: { type: Schema.Types.ObjectId, ref: LICENSE_MODEL, required: true },
-  type: { type: String, required: true },
+  type: { type: String, required: true, default: FREE },
   price: { type: Number, default: 0, required: true },
   currency: { type: String, required: true },
-  maxSize: { type: Number, default: 0 },
-  maxSizeT: { type: String, default: '0 B' },
-  expire: { type: Date, required: true },
+  maxSize: { type: Number, default: B500MB },
+  maxSizeT: { type: String, default: convertBytes(B500MB) },
+  expire: { type: Date, default: moment().add(1, 'year') },
   enabled: { type: Boolean, default: true },
+  requestsPerMonth: { type: Number, default: REQUESTS_PER_MONTH }
 }, {
   timestamps: true
 })
--- src/interfaces/subscription.interface.ts
+++ src/interfaces/subscription.interface.ts
@@ -2,13 +2,16 @@ export interface ISubscription {
   _id?: string
   user?: string
   license?: string
-  type: string
+  type: SubscriptionType
   price: number
   currency: string
-  maxSize?: number
-  maxSizeT?: string
-  expire?: Date
+  maxSize: number
+  maxSizeT: string
+  expire: Date
   enabled: boolean
+  requestsPerMonth: number
+  createdAt: Date
+  updatedAt: Date
   __v?: number | string | undefined
 }
 

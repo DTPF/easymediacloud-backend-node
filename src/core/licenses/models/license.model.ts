2960fcdc22cb29fbd830694a497e714a06c7e4c2
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-27 00:01:19 +0100

    change: add requests field in licenses model

--- src/core/licenses/models/license.model.ts
+++ src/core/licenses/models/license.model.ts
@@ -3,14 +3,15 @@ const { Schema, model } = require('mongoose');
 
 const LicenseSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: USER_MODEL },
+  subscription: { type: Schema.Types.ObjectId, ref: SUBSCRIPTION_MODEL },
   project: { type: String, required: true },
   apiKey: { type: String, required: true, unique: true },
   enabled: { type: Boolean, required: true, default: true },
-  online: { type: Boolean, required: true },
+  online: { type: Boolean, required: true, default: true },
   size: { type: Number, default: 0 },
   sizeT: { type: String, default: '0 B' },
   totalFiles: { type: Number, default: 0 },
-  subscription: { type: Schema.Types.ObjectId, ref: SUBSCRIPTION_MODEL },
+  requests: { type: Number, default: 0 },
 }, {
   timestamps: true
 })

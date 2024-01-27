4daa58b80df7321c83e38bf9efb26ac0c2897452
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-27 13:19:04 +0100

    change: add fields to token

--- src/services/jwt.ts
+++ src/services/jwt.ts
@@ -2,25 +2,26 @@ import jwt from "jsonwebtoken";
 import { ServerConfig } from "../config/config";
 import mongoose from "mongoose";
 import moment from "moment";
-import { IApiKey } from "../interfaces/license.interface";
+import { IApiKeyToken } from "../interfaces/license.interface";
 const config: ServerConfig = require('../config/config')
 
-export async function createLicenseApiKeyJWT(project: string, mainFolderName: string | Object) {
+export async function createLicenseApiKeyJWT(project: string, mainFolderName: string) {
   const payload = {
-    id: mainFolderName.toString(),
+    id: mainFolderName,
     project: project,
     apiKey: new mongoose.Types.ObjectId,
-    createToken: moment().unix(),
+    createdAt: moment().unix(),
+    updatedAt: moment().unix(),
   };
   if (!config.app.SECRET_KEY) throw new Error('SECRET_KEY is not defined')
   return jwt.sign(payload, config.app.SECRET_KEY as string);
 }
 
-export async function refreshLicenseApiKeyJWT(oldJwt: IApiKey) {
+export async function refreshLicenseApiKeyJWT(oldJwt: IApiKeyToken) {
   const payload = {
     ...oldJwt,
     apiKey: new mongoose.Types.ObjectId,
-    createToken: moment().unix(),
+    updatedAt: moment().unix(),
   };
   if (!config.app.SECRET_KEY) throw new Error('SECRET_KEY is not defined')
   return jwt.sign(payload, config.app.SECRET_KEY as string);
